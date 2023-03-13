b9ad0bbb55c306bfa1f58d6efe39c794d6d890ff
zero-id <abdullatiffebriliana15@gmail.com>
2023-03-13 19:00:50 +0700

    tugas 20 js

---
+++ tugas20.js
@@ -0,0 +1,8 @@
+function tugasduapuluh() {
+    var kata = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+
+    console.log(kata.match("are"))
+    console.log(kata.match("words"))
+}
+
+tugasduapuluh()
